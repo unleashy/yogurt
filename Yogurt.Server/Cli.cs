82f6da9d2faae319d5fedbdd5adb02761fa6d6ca
bnuuy.bsky.social <me@emmanuelle.dev>
2026-07-01 02:46:03 -0300

    [Server] Add basic CLI

---
+++ Yogurt.Server/Cli.cs
@@ -0,0 +1,108 @@
+﻿using System.CommandLine;
+using System.CommandLine.Parsing;
+using System.Diagnostics;
+
+namespace Yogurt.Server;
+
+internal sealed class Cli
+{
+    public readonly struct Parsing(Cli cli, ParseResult parseResult)
+    {
+        public bool TryInvokeAction(out int exitCode)
+        {
+            exitCode = parseResult.Invoke();
+            return parseResult.Action is not null;
+        }
+
+        public uint? ClientProcessId => parseResult.GetValue(cli._clientProcessId);
+
+        public TransportOption Transport {
+            get {
+                Debug.Assert(!cli.HasConflictingTransportOptions(parseResult.RootCommandResult));
+
+                if (parseResult.GetValue(cli._pipe) is {} fileInfo) {
+                    return new TransportOption.Pipe(fileInfo);
+                }
+                else if (parseResult.GetValue(cli._socket) is {} port) {
+                    return new TransportOption.Socket(port);
+                }
+                else {
+                    return new TransportOption.Console();
+                }
+            }
+        }
+    }
+
+    public abstract record TransportOption
+    {
+        public sealed record Console : TransportOption;
+        public sealed record Socket(uint Port) : TransportOption;
+        public sealed record Pipe(FileInfo Path) : TransportOption;
+
+        private TransportOption() {}
+    }
+
+    private readonly RootCommand _command;
+
+    private readonly Option<uint?> _clientProcessId = new("--clientProcessId") {
+        Description = "The process ID of the client using this LSP",
+        HelpName = "pid",
+    };
+
+    private readonly Option<bool?> _stdio = new("--stdio") {
+        Description =
+            """
+            Use standard input/output to communicate with the client. If no communication
+            flag is supplied, this is the default.
+            """,
+        Arity = ArgumentArity.Zero,
+    };
+
+    private readonly Option<FileInfo?> _pipe = new("--pipe") {
+        Description = "Use the given named pipe/domain socket to communicate with the client.",
+        HelpName = "path",
+    };
+
+    private readonly Option<ushort?> _socket = new("--socket") {
+        Description =
+            """
+            Use a TCP socket at localhost on the given port to communicate with
+            the client.
+            """,
+        HelpName = "port",
+    };
+
+    public static Parsing Parse(IReadOnlyList<string> args)
+    {
+        var cli = new Cli();
+        var parseResult = cli._command.Parse(args);
+        return new Parsing(cli, parseResult);
+    }
+
+    private Cli()
+    {
+        _command = new RootCommand("Yogurt LSP server") {
+            _clientProcessId,
+            _stdio,
+            _pipe.AcceptLegalFilePathsOnly(),
+            _socket,
+        };
+        _command.Validators.Add(Validate);
+    }
+
+    private void Validate(CommandResult cli)
+    {
+        if (HasConflictingTransportOptions(cli)) {
+            cli.AddError("Only one of --stdio, --pipe, or --socket may be specified at once.");
+        }
+    }
+
+    private bool HasConflictingTransportOptions(CommandResult cli)
+    {
+        var a = cli.GetResult(_stdio);
+        var b = cli.GetResult(_pipe);
+        var c = cli.GetResult(_socket);
+
+        return (a != null && b != null) || (a != null && c != null) || (b != null && c != null);
+    }
+}
--- Yogurt.Server/Program.cs
+++ Yogurt.Server/Program.cs
@@ -1,1 +1,11 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using Yogurt.Server;
+
+var result = Cli.Parse(args);
+if (result.TryInvokeAction(out var exitCode)) {
+    return exitCode;
+}
+
+Console.WriteLine($"Client process ID = {result.ClientProcessId?.ToString() ?? "null"}");
+Console.WriteLine($"Transport = {result.Transport}");
+
+return 0;
