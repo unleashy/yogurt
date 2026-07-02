0f4643376bd4c96f597eed6120c65a26e5da1740
bnuuy.bsky.social <me@emmanuelle.dev>
2026-07-02 02:22:48 -0300

    [Vscode] Connect to server on activate

--- Yogurt.Vscode/src/extension.ts
+++ Yogurt.Vscode/src/extension.ts
@@ -1,6 +1,36 @@
 import * as vscode from "vscode";
+import {
+  LanguageClient,
+  type LanguageClientOptions,
+  type ServerOptions,
+  TransportKind,
+} from "vscode-languageclient/node";
 
-export function activate(context: vscode.ExtensionContext) {
-  let serverPath = vscode.workspace.getConfiguration("yogurt").get("serverPath") as string;
+let client: LanguageClient;
+
+export async function activate(context: vscode.ExtensionContext): Promise<void> {
+  let config = vscode.workspace.getConfiguration("yogurt");
+
+  let serverPath = config.get("serverPath") as string;
   console.log(`[yogurt] ${serverPath}`);
+
+  let serverOptions: ServerOptions = {
+    command: serverPath,
+    transport: TransportKind.stdio,
+  };
+
+  let clientOptions: LanguageClientOptions = {
+    documentSelector: [{ scheme: "file", language: "yogurt" }],
+  };
+
+  client = new LanguageClient("yogurt", "Yogurt", serverOptions, clientOptions);
+
+  await client.start();
+}
+
+export function deactivate(): Promise<void> | undefined {
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  if (client) {
+    return client.stop();
+  }
 }
