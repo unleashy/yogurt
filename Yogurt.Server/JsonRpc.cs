68d0125d2735d275b9b1f94974b814ba735cd527
bnuuy.bsky.social <me@emmanuelle.dev>
2026-07-10 17:57:18 -0300

    [Server] Add JsonRpcRequest and parse it

---
+++ Yogurt.Server/JsonRpc.cs
@@ -0,0 +1,102 @@
+﻿using Yogurt.Json;
+
+namespace Yogurt.Server;
+
+public static class JsonRpc
+{
+    [PublicAPI]
+    public static JsonRpcRequest? TryParseRequest(JsonValue json) =>
+        json.TryObject(
+            new JsonRpcRequest(),
+            new JsonObjectShape<JsonRpcRequest>()
+                .Require("jsonrpc", static json => json.TryLiteral("2.0"))
+                .Require("method",
+                    static json => json.TryString(),
+                    static (method, req) => req with { Method = method }
+                )
+                .Allow("id",
+                    TryParseId,
+                    static (id, req) => req with { Id = id }
+                )
+                .Allow("params",
+                    static json => json.TryStructuralValue(),
+                    static (@params, req) => req with { Params = @params }
+                )
+        );
+
+    private static JsonRpcId? TryParseId(JsonValue json) =>
+        json.TryNumber<int>() is {} ival ? JsonRpcId.Int(ival)
+        : json.TryString() is {} sval ? JsonRpcId.String(sval)
+        : json.TryNull() ? JsonRpcId.Null
+        : null;
+}
+
+public readonly struct JsonRpcId : IEquatable<JsonRpcId>
+{
+    private enum Repr
+    {
+        Null,
+        Integer,
+        String,
+    }
+
+    private readonly Repr _repr = Repr.Null;
+    private readonly int _ival = 0;
+    private readonly string? _sval = null;
+
+    [PublicAPI]
+    public static readonly JsonRpcId Null;
+
+    [PublicAPI]
+    public static JsonRpcId Int(int id) => new(id);
+
+    [PublicAPI]
+    public static JsonRpcId String(string id) => new(id);
+
+    private JsonRpcId(int id)
+    {
+        _repr = Repr.Integer;
+        _ival = id;
+    }
+
+    private JsonRpcId(string id)
+    {
+        _repr = Repr.String;
+        _sval = id;
+    }
+
+    [PublicAPI]
+    public bool Equals(JsonRpcId other) => (_repr, other._repr) switch {
+        (Repr.Null,    Repr.Null)    => true,
+        (Repr.Integer, Repr.Integer) => _ival == other._ival,
+        (Repr.String,  Repr.String)  => _sval == other._sval,
+
+        _ => false,
+    };
+
+    [PublicAPI]
+    public override bool Equals(object? obj) => obj is JsonRpcId other && Equals(other);
+
+    [PublicAPI]
+    public override int GetHashCode() => _repr switch {
+        Repr.Null    => HashCode.Combine(_repr),
+        Repr.Integer => HashCode.Combine(_repr, _ival),
+        Repr.String  => HashCode.Combine(_repr, _sval),
+    };
+
+    [PublicAPI]
+    public static bool operator ==(JsonRpcId left, JsonRpcId right) => left.Equals(right);
+
+    [PublicAPI]
+    public static bool operator !=(JsonRpcId left, JsonRpcId right) => !left.Equals(right);
+
+    [PublicAPI]
+    public override string ToString() => _repr switch {
+        Repr.Null    => "JsonRpcId(null)",
+        Repr.Integer => $"JsonRpcId({_ival})",
+        Repr.String  => $"JsonRpcId({_sval})",
+    };
+}
+
+[PublicAPI]
+public readonly record struct JsonRpcRequest(JsonRpcId? Id, string Method, JsonValue? Params);
---
+++ Yogurt.Tests/JsonRpcRequestTests.cs
@@ -0,0 +1,153 @@
+﻿using Yogurt.Json;
+using Yogurt.Server;
+
+namespace Yogurt.Tests;
+
+public class JsonRpcRequestTests
+{
+    [Test]
+    public void RequestNoParams()
+    {
+        var json = JsonValue.Parse(
+            """
+            { "jsonrpc": "2.0"
+            , "method": "no-params"
+            , "id": 0 }
+            """
+        );
+
+        var sut = JsonRpc.TryParseRequest(json);
+
+        Assert.That(
+            sut,
+            Is.EqualTo(new JsonRpcRequest {
+                Id     = JsonRpcId.Int(0),
+                Method = "no-params",
+                Params = null,
+            })
+        );
+    }
+
+    [Test]
+    public void RequestNullId()
+    {
+        var json = JsonValue.Parse(
+            """
+            { "jsonrpc": "2.0"
+            , "method": "null-id"
+            , "id": null }
+            """
+        );
+
+        var sut = JsonRpc.TryParseRequest(json);
+
+        Assert.That(
+            sut,
+            Is.EqualTo(new JsonRpcRequest {
+                Id     = JsonRpcId.Null,
+                Method = "null-id",
+                Params = null,
+            })
+        );
+    }
+
+    [Test]
+    public void RequestStringId()
+    {
+        var json = JsonValue.Parse(
+            """
+            { "jsonrpc": "2.0"
+            , "method": "string-id"
+            , "id": "🆔" }
+            """
+        );
+
+        var sut = JsonRpc.TryParseRequest(json);
+
+        Assert.That(
+            sut,
+            Is.EqualTo(new JsonRpcRequest {
+                Id     = JsonRpcId.String("🆔"),
+                Method = "string-id",
+                Params = null,
+            })
+        );
+    }
+
+    [Test]
+    public void RequestNotification()
+    {
+        var json = JsonValue.Parse(
+            """
+            { "jsonrpc": "2.0"
+            , "method": "notification" }
+            """
+        );
+
+        var sut = JsonRpc.TryParseRequest(json);
+
+        Assert.That(
+            sut,
+            Is.EqualTo(new JsonRpcRequest {
+                Id     = null,
+                Method = "notification",
+                Params = null,
+            })
+        );
+    }
+
+    [Test]
+    public void RequestWithParams()
+    {
+        var json = JsonValue.Parse(
+            """
+            { "jsonrpc": "2.0"
+            , "method": "with-params"
+            , "id": 123
+            , "params": [1, 2, 3] }
+            """
+        );
+
+        var sut = JsonRpc.TryParseRequest(json)!.Value;
+
+        using (Assert.EnterMultipleScope()) {
+            Assert.That(sut.Id,     Is.EqualTo(JsonRpcId.Int(123)));
+            Assert.That(sut.Method, Is.EqualTo("with-params"));
+            Assert.That(
+                sut.Params?.TryArray(static json => json.TryNumber<int>()),
+                Is.EqualTo([1, 2, 3])
+            );
+        }
+    }
+
+    [Test]
+    public void RequestWithInvalidParams()
+    {
+        var json = JsonValue.Parse(
+            """
+            { "jsonrpc": "2.0"
+            , "method": "invalid-params"
+            , "params": 0 }
+            """
+        );
+
+        var sut = JsonRpc.TryParseRequest(json);
+
+        Assert.That(sut, Is.Null);
+    }
+
+    [Test]
+    public void RequestWithInvalidVersion()
+    {
+        var json = JsonValue.Parse(
+            """
+            { "jsonrpc": "1.0"
+            , "method": "invalid-version" }
+            """
+        );
+
+        var sut = JsonRpc.TryParseRequest(json);
+
+        Assert.That(sut, Is.Null);
+    }
+}
