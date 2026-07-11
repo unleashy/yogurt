128edef2a3b73c9a53ceb7052db2dfe363270d35
bnuuy.bsky.social <me@emmanuelle.dev>
2026-07-11 18:14:42 -0300

    [Server] Implement ProtocolWriter

---
+++ Yogurt.Server/ProtocolWriter.cs
@@ -0,0 +1,32 @@
+﻿using System.Globalization;
+using System.IO.Pipelines;
+
+namespace Yogurt.Server;
+
+public sealed class ProtocolWriter(PipeWriter writer) : IProtocolWriter
+{
+    public async ValueTask WriteAsync(
+        ProtocolMessage message,
+        CancellationToken cancellationToken = default
+    )
+    {
+        if (cancellationToken.IsCancellationRequested) return;
+
+        var header = "Content-Length: "u8;
+        var separator = "\r\n\r\n"u8;
+        var messageLength = message.Utf8Text.Length;
+        var numDigits = (int)(1 + float.Max(0, float.Log10(messageLength)));
+        var headersLength = header.Length + numDigits + separator.Length;
+
+        var span = writer.GetSpan(headersLength);
+
+        header.CopyTo(span);
+        _ = messageLength
+            .TryFormat(span[header.Length ..], out _, provider: CultureInfo.InvariantCulture);
+        separator.CopyTo(span[(header.Length + numDigits) ..]);
+
+        writer.Advance(headersLength);
+
+        _ = await writer.WriteAsync(message.Utf8Text, cancellationToken);
+    }
+}
---
+++ Yogurt.Tests/ProtocolWriterTests.cs
@@ -0,0 +1,69 @@
+﻿using System.IO.Pipelines;
+using System.Text;
+using Yogurt.Server;
+
+namespace Yogurt.Tests;
+
+public class ProtocolWriterTests
+{
+    private static ProtocolMessage Message(string s) => new(Encoding.UTF8.GetBytes(s));
+
+    private static async ValueTask<string> ReadAllAsync(PipeReader reader)
+    {
+        var result = await reader.ReadAtLeastAsync(int.MaxValue);
+        return Encoding.UTF8.GetString(result.Buffer);
+    }
+
+    [Test]
+    public async Task Empty()
+    {
+        var pipe = new Pipe();
+        var sut = new ProtocolWriter(pipe.Writer);
+
+        await sut.WriteAsync(Message(""));
+        await pipe.Writer.CompleteAsync();
+
+        var data = await ReadAllAsync(pipe.Reader);
+        Assert.That(data, Is.EqualTo("Content-Length: 0\r\n\r\n"));
+    }
+
+    [Test]
+    public async Task Simple()
+    {
+        var pipe = new Pipe();
+        var sut = new ProtocolWriter(pipe.Writer);
+
+        await sut.WriteAsync(Message("abcd\nefgh"));
+        await pipe.Writer.CompleteAsync();
+
+        var data = await ReadAllAsync(pipe.Reader);
+        Assert.That(data, Is.EqualTo("Content-Length: 9\r\n\r\nabcd\nefgh"));
+    }
+
+    [Test]
+    public async Task Utf8()
+    {
+        var pipe = new Pipe();
+        var sut = new ProtocolWriter(pipe.Writer);
+
+        await sut.WriteAsync(Message("bunny 🐇"));
+        await pipe.Writer.CompleteAsync();
+
+        var data = await ReadAllAsync(pipe.Reader);
+        Assert.That(data, Is.EqualTo("Content-Length: 10\r\n\r\nbunny 🐇"));
+    }
+
+    [Test]
+    public async Task VeryLong()
+    {
+        var pipe = new Pipe();
+        var sut = new ProtocolWriter(pipe.Writer);
+        var message = string.Join("", Enumerable.Repeat("message", 1234));
+
+        await sut.WriteAsync(Message(message));
+        await pipe.Writer.CompleteAsync();
+
+        var data = await ReadAllAsync(pipe.Reader);
+        Assert.That(data, Is.EqualTo($"Content-Length: 8638\r\n\r\n{message}"));
+    }
+}
