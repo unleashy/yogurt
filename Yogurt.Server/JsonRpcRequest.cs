21c3b8b4a99cd3418fbd4d253b0963f0f651087b
bnuuy.bsky.social <me@emmanuelle.dev>
2026-07-10 23:09:27 -0300

    [Server] Move JsonRpc classes to own files

---
+++ Yogurt.Server/JsonRpcRequest.cs
@@ -0,0 +1,27 @@
+﻿using Yogurt.Json;
+
+namespace Yogurt.Server;
+
+[PublicAPI]
+public readonly record struct JsonRpcRequest(JsonRpcId? Id, string Method, JsonValue? Params)
+{
+    [PublicAPI]
+    public static JsonRpcRequest? TryParse(JsonValue json) =>
+        json.TryObject(
+            new JsonRpcRequest(),
+            new JsonObjectShape<JsonRpcRequest>()
+                .Require("jsonrpc", static json => json.TryLiteral("2.0"))
+                .Require("method",
+                    static json => json.TryString(),
+                    static (method, req) => req with { Method = method }
+                )
+                .Allow("id",
+                    JsonRpcId.TryParse,
+                    static (id, req) => req with { Id = id }
+                )
+                .Allow("params",
+                    static json => json.TryStructuralValue(),
+                    static (@params, req) => req with { Params = @params }
+                )
+        );
+}
