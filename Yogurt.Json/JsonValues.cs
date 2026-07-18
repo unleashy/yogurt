3ccaa24622657b43b9993b49d3f097858d52c15d
bnuuy.bsky.social <me@emmanuelle.dev>
2026-07-17 23:05:16 -0300

    [Json] Add a way to get simple JsonValues easily

---
+++ Yogurt.Json/JsonValues.cs
@@ -0,0 +1,27 @@
+﻿namespace Yogurt.Json;
+
+public static class JsonValues
+{
+    [PublicAPI]
+    public static JsonValue Null { get; } = CreateNullValue();
+
+    [PublicAPI]
+    public static JsonValue True { get; } = CreateBooleanValue(true);
+
+    [PublicAPI]
+    public static JsonValue False { get; } = CreateBooleanValue(false);
+
+    private static JsonValue CreateBooleanValue(bool value)
+    {
+        var b = new JsonBuilder();
+        b.Boolean(value);
+        return b.Build();
+    }
+
+    private static JsonValue CreateNullValue()
+    {
+        var b = new JsonBuilder();
+        b.Null();
+        return b.Build();
+    }
+}
