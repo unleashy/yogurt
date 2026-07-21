f96ed5b6530518e14c076bf0f9309d052ec6b450
bnuuy.bsky.social <me@emmanuelle.dev>
2026-07-21 04:34:15 -0300

    [JsonRpc] Add Client and tests

---
+++ Yogurt.JsonRpc/JsonRpcClient.cs
@@ -0,0 +1,132 @@
+﻿using System.Collections.Concurrent;
+using System.Diagnostics;
+
+namespace Yogurt.JsonRpc;
+
+public sealed class JsonRpcClient(IJsonRpcChannel channel) : IJsonRpcObserver
+{
+    private readonly record struct PendingRequest(TaskCompletionSource<JsonRpcResponse> Tcs);
+
+    private readonly ConcurrentDictionary<JsonRpcId, PendingRequest> _pendingRequests = new();
+
+    [PublicAPI]
+    public Func<JsonRpcId> IdGenerator { get; set; } = DefaultIdGenerator.Generate;
+
+    [PublicAPI]
+    public Task<JsonRpcResponse> InvokeAsync(
+        string method,
+        JsonValue? parameters = null,
+        CancellationToken cancellationToken = default
+    )
+    {
+        if (cancellationToken.IsCancellationRequested) {
+            return Task.FromCanceled<JsonRpcResponse>(cancellationToken);
+        }
+
+        var id = IdGenerator();
+        var request = new JsonRpcRequest(id, method, parameters);
+        var message = JsonRpcMessage.Request(request);
+
+        var responseTask = RegisterPending(id, cancellationToken);
+        return InvokeAsyncCore(message, responseTask, cancellationToken);
+    }
+
+    private async Task<JsonRpcResponse> InvokeAsyncCore(
+        JsonRpcMessage message,
+        Task<JsonRpcResponse> responseTask,
+        CancellationToken cancellationToken
+    )
+    {
+        await channel.Output.WriteAsync(message, cancellationToken);
+        return await responseTask;
+    }
+
+    [PublicAPI]
+    public ValueTask NotifyAsync(
+        string method,
+        JsonValue? parameters = null,
+        CancellationToken cancellationToken = default
+    )
+    {
+        var request = new JsonRpcRequest(id: null, method, parameters);
+        var message = JsonRpcMessage.Request(request);
+        return channel.Output.WriteAsync(message, cancellationToken);
+    }
+
+    [PublicAPI]
+    public ValueTask<bool> OnResponseAsync(
+        JsonRpcResponse response,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return ValueTask.FromResult(CompletePending(response));
+    }
+
+    private Task<JsonRpcResponse> RegisterPending(JsonRpcId id, CancellationToken ct)
+    {
+        var tcs = new TaskCompletionSource<JsonRpcResponse>(
+            TaskCreationOptions.RunContinuationsAsynchronously
+        );
+
+        if (!_pendingRequests.TryAdd(id, new PendingRequest(tcs))) {
+            throw new UnreachableException(
+                $"pending request duplicate ID {id}; this should never happen!"
+            );
+        }
+
+        if (ct.CanBeCanceled) {
+            var reg = ct.Register(() => CancelPending(id, tcs, ct));
+            _ = tcs.Task.ContinueWith(_ => reg.Dispose(), CancellationToken.None);
+        }
+
+        return tcs.Task;
+    }
+
+    private bool CompletePending(JsonRpcResponse response)
+    {
+        if (!_pendingRequests.TryRemove(response.Id, out var r)) {
+            return false;
+        }
+
+        // The only way this fails is if the TCS's Task was cancelled (via the Registration).
+        // Note that we do not need to dispose the Registration because we use a continuation on
+        // the TCS's Task that does so
+        if (!r.Tcs.TrySetResult(response)) {
+            Debug.Assert(r.Tcs.Task.IsCanceled);
+        }
+
+        return true;
+    }
+
+    private void CancelPending(
+        JsonRpcId id,
+        TaskCompletionSource<JsonRpcResponse> tcs,
+        CancellationToken ct
+    )
+    {
+        _ = _pendingRequests.TryRemove(id, out _);
+        _ = tcs.TrySetCanceled(ct);
+    }
+
+    ValueTask<bool> IJsonRpcObserver.OnRequestAsync(
+        JsonRpcRequest request,
+        CancellationToken cancellationToken
+    )
+    {
+        return ValueTask.FromResult(false);
+    }
+}
+
+file static class DefaultIdGenerator
+{
+    [ThreadStatic] private static Random? _random;
+
+    public static JsonRpcId Generate()
+    {
+        var random = _random ??= new Random();
+
+        var s = random.GetString("0123456789abcdefghijklmnopqrstuvwxyz", length: 12);
+
+        return JsonRpcId.String(s);
+    }
+}
---
+++ Yogurt.Tests/JsonRpc/ClientTests.cs
@@ -0,0 +1,119 @@
+﻿using Yogurt.Json;
+using Yogurt.JsonRpc;
+
+namespace Yogurt.Tests.JsonRpc;
+
+public class ClientTests
+{
+    [Test]
+    public async Task Invoke_WritesRequest_AwaitsResponse()
+    {
+        var channel = new FakeChannel();
+        var sut = new JsonRpcClient(channel) { IdGenerator = () => JsonRpcId.Int(0) };
+        var request = new JsonRpcRequest { Id = sut.IdGenerator(), Method = ":p" };
+
+        var task = sut.InvokeAsync(request.Method);
+        var message = await channel.OutputReader.ReadAsync();
+
+        using (Assert.EnterMultipleScope()) {
+            Assert.That(message, Is.EqualTo(JsonRpcMessage.Request(request)));
+            Assert.That(task.IsCompleted, Is.False);
+        }
+    }
+
+    [Test]
+    public async Task Invoke_WritesRequest_ReceivesResponse()
+    {
+        var channel = new FakeChannel();
+        var sut = new JsonRpcClient(channel) { IdGenerator = () => JsonRpcId.Int(0) };
+        var request = new JsonRpcRequest { Id = sut.IdGenerator(), Method = ":p" };
+        var response = JsonRpcResponse.Result(sut.IdGenerator(), JsonValues.True);
+
+        var task = sut.InvokeAsync(request.Method);
+        var message = await channel.OutputReader.ReadAsync();
+        var handled = await sut.OnResponseAsync(response);
+
+        using (Assert.EnterMultipleScope()) {
+            Assert.That(handled, Is.True);
+            Assert.That(message, Is.EqualTo(JsonRpcMessage.Request(request)));
+            Assert.That(await task, Is.EqualTo(response));
+        }
+    }
+
+    [Test]
+    public async Task Invoke_EarlyCancellation_Fails()
+    {
+        var channel = new FakeChannel();
+        var sut = new JsonRpcClient(channel);
+        using var cts = new CancellationTokenSource();
+
+        await cts.CancelAsync();
+        var task = sut.InvokeAsync("cancel", cancellationToken: cts.Token);
+
+        using (Assert.EnterMultipleScope()) {
+            await Assert.ThatAsync(
+                () => task,
+                Throws
+                    .InstanceOf<OperationCanceledException>().And
+                    .Property("CancellationToken").EqualTo(cts.Token)
+            );
+            Assert.That(channel.OutputReader.Count, Is.Zero);
+        }
+    }
+
+    [Test]
+    public async Task Invoke_MidwayCancellation_Fails()
+    {
+        var channel = new FakeChannel();
+        var sut = new JsonRpcClient(channel);
+        using var cts = new CancellationTokenSource();
+
+        var task = sut.InvokeAsync("cancel", cancellationToken: cts.Token);
+        await cts.CancelAsync();
+
+        await Assert.ThatAsync(
+            () => task,
+            Throws
+                .InstanceOf<OperationCanceledException>().And
+                .Property("CancellationToken").EqualTo(cts.Token)
+        );
+    }
+
+    [Test]
+    public async Task OnResponse_UnknownId_Rejects()
+    {
+        var channel = new FakeChannel();
+        var sut = new JsonRpcClient(channel);
+
+        var handled = await sut.OnResponseAsync(
+            JsonRpcResponse.Result(JsonRpcId.Int(1234), JsonValues.False)
+        );
+
+        Assert.That(handled, Is.False);
+    }
+
+    [Test]
+    public async Task Notify_WritesRequest()
+    {
+        var channel = new FakeChannel();
+        var sut = new JsonRpcClient(channel);
+
+        await sut.NotifyAsync("➡️");
+
+        var message = await channel.OutputReader.ReadAsync();
+        Assert.That(message, Is.EqualTo(
+            JsonRpcMessage.Request(new JsonRpcRequest { Method = "➡️" })
+        ));
+    }
+
+    [Test]
+    public async Task OnRequest_Rejects()
+    {
+        var channel = new FakeChannel();
+        var sut = new JsonRpcClient(channel);
+
+        var handled = await ((IJsonRpcObserver) sut).OnRequestAsync(default);
+
+        Assert.That(handled, Is.False);
+    }
+}
