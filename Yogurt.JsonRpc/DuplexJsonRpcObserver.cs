7aee28ec92d12a9b55f5f74047f83d23d0fd087e
bnuuy.bsky.social <me@emmanuelle.dev>
2026-07-21 17:24:48 -0300

    [JsonRpc] Add duplex observer forwarding to Client/Server

---
+++ Yogurt.JsonRpc/DuplexJsonRpcObserver.cs
@@ -0,0 +1,21 @@
+﻿namespace Yogurt.JsonRpc;
+
+public sealed class DuplexJsonRpcObserver(JsonRpcClient client, JsonRpcServer server)
+    : IJsonRpcObserver
+{
+    public ValueTask<bool> OnRequestAsync(
+        JsonRpcRequest request,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return server.OnRequestAsync(request, cancellationToken);
+    }
+
+    public ValueTask<bool> OnResponseAsync(
+        JsonRpcResponse response,
+        CancellationToken cancellationToken = default
+    )
+    {
+        return client.OnResponseAsync(response, cancellationToken);
+    }
+}
